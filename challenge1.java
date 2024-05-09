c6b9a5781702960d09eabccf743b0ae735282353
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-05-09 11:53:41 -0500

    working on challenge 1

---
+++ challenge1.java
@@ -0,0 +1,9 @@
+
+//Write a function that takes an integer minutes and converts it to seconds.
+
+public class challenge1 {
+    public static int convert(int minutes) {
+    convert= minutes/60;
+    return convert(int);
+    };
+}
