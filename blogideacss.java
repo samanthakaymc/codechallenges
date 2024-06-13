540ed92e6a06175c6853e3782c22d3e9cd256b28
sammcmurtrey <mcmurtreysamantha@gmail.com>
2024-06-13 16:52:03 -0500

    css ideas

--- blogideacss.java
+++ blogideacss.java
@@ -1,5 +1,8 @@
 
 //want to make a public blog, cooking recipes, movie and tv recs. want to design it usnng css
 
+//need server & url. want to host css here or different ?
 public class blogideacss {
+
+
 }
